cb9b9dba8b8d14676b1c12326dbe9544fb19d864
Davide <davidefiorini96@gmail.com>
2022-09-12 10:40:01 +0200

    fix comment and change folder name

--- functions/base.ts
+++ functions/base.ts
@@ -1,6 +1,6 @@
 import Airtable from "airtable";
 
-// api keys are confidential
+/** retrieve data from airtable base --- remember api keys are confidential */
 const base = new Airtable({ apiKey: process.env.AIRTABLE_API_KEY }).base(
   "app9vHt85pjJFeOik"
 );
